490dec3851cc915ef4fb0d62bc1e40c21a6ab0af
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-05-07 20:50:35 +0330

    add custom color ,font ,bgc

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 export default function App() {
   return (
-    <div>
+    <div className=''>
       <Header></Header>
     </div>
   )
--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -1,11 +1,14 @@
 const NavLinks = () => {
     return (
-      <nav className="hidden md:flex gap-6 font-medium text-gray-600">
-        <a href="#" className="hover:text-black">Home</a>
-        <a href="#" className="hover:text-black">Shop</a>
-        <a href="#" className="hover:text-black">About</a>
-        <a href="#" className="hover:text-black">Contact</a>
-      </nav>
+      <>
+        <h1 className="">Welcome to website FitLand!</h1>
+        <nav className="hidden md:flex gap-6 font-medium text-gray-600">
+          <a href="#" className="hover:text-black">Home</a>
+          <a href="#" className="hover:text-black">Shop</a>
+          <a href="#" className="hover:text-black">About</a>
+          <a href="#" className="hover:text-black">Contact</a>
+        </nav>
+      </>
     );
   };
   
