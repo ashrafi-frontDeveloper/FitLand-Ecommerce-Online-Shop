4b43b8e0afc6f24c133f6922c71808cf54b24c6f
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-05-08 22:30:53 +0330

    style(container) change gap-x for 2xl and xl size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="fixed flex items-center justify-center h-10 w-full font-vazirB font-medium text-sm text-white bg-secondary">
         <a href="#">:) با عضویت در فیت لند اولین ارسال را مهمون ما باش</a>
       </div>
-      <header className="container pt-12 mx-auto w-full">
+      <header className="container p-0 2xl:px-10 pt-12 mx-auto w-full">
         <div className="container mx-auto px-4 py-3 flex items-center justify-between">
           <Logo /> 
           {/* <NavLinks /> */}
