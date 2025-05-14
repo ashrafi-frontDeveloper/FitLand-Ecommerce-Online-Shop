5ebba0c9cfac738f6ed113823aa1ec0a6b5c149c
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-05-14 20:24:08 +0330

    feat(SpecialDiscounts): dynamic product

--- src/components/Main/SpecialDiscounts.jsx
+++ src/components/Main/SpecialDiscounts.jsx
@@ -1,6 +1,15 @@
 import { toPersianDigits } from "../../utils/ConvertToPersian"
+import { FcVip } from "react-icons/fc";
+
+const products = [
+    {id: 1, img: 'images/discound-product/img1.png' , name: 'سویشرت نایک مدل AO-14' , price: 300_000 , size: 'از سایز L تا XXL'},
+    {id: 2, img: 'images/discound-product/img2.jpg' , name: 'ست نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL'},
+    {id: 3, img: 'images/discound-product/img3.png', name: 'کفش نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL'},
+]
 
 const SpecialDiscounts = () => {
+
+
     return (
         <>
             <div className="container h-[600px] px-0 xl:px-4 p-0 2xl:px-10 pt-8 mx-auto mb-20 w-full">
@@ -9,25 +18,32 @@ const SpecialDiscounts = () => {
                     <span className=" absolute -top-14 rounded-3xl text-2xl 2xl:text-3xl font-vazirB font-bold bg-white px-10 py-2.5">تخفیفات ویژه</span>
                 </div>
                 {/* products */}
-                <div className="">
+                <div className="flex items-center justify-between">
                     {/* product */}
-                    <div className="w-[366px] h-[467px] mt-20">
-                        {/* img */}
-                        <img src="images/discound-product/img2.jpg" alt="" className="" />
-                        {/* details */}
-                        <div className="mt-4">
-                            <div className="flex flex-col gap-y-2">
-                                <h6 className="" >سویشرت نایک مدل AO-14</h6>
-                                <span>{toPersianDigits(300000)} تومان</span>
-                                <span>از سایز L تا XXL</span>
-                            </div>
-                            <div className="flex relative mt-2">
-                                <span className=" absolute right-6 inline-block w-5 h-5 rounded-full bg-black"></span>
-                                <span className=" absolute right-3 inline-block w-5 h-5 rounded-full bg-NeutralSecondory"></span>
-                                <span className=" absolute inline-block w-5 h-5 rounded-full bg-black"></span>
+                    {
+                    products.map(product => (
+
+                        <div className=" relative w-[366px] h-[467px] mt-20 bg-white rounded-4xl">
+                            {/* icon discound */}
+                            <div className="absolute left-2 top-2.5 "><FcVip className="w-10 h-10" /></div>
+                            {/* img */}
+                            <img src={product.img} alt="" className=" rounded-4xl" />
+                            {/* details */}
+                            <div className="mt-4 p-4">
+                                <div className="flex flex-col gap-y-2">
+                                    <h6 className="" >{product.name}</h6>
+                                    <span>{toPersianDigits(product.price)} تومان</span>
+                                    <span>{product.size}</span>
+                                </div>
+                                <div className="flex relative mt-2">
+                                    <span className=" absolute right-6 inline-block w-5 h-5 rounded-full bg-black"></span>
+                                    <span className=" absolute right-3 inline-block w-5 h-5 rounded-full bg-secondaryCBCBCB"></span>
+                                    <span className=" absolute inline-block w-5 h-5 rounded-full bg-black"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ))
+                    }
                 </div>
             </div>
         </>
