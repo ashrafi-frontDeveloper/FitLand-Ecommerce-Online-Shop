2b8848bc6923de40b50f7bc417b4b9d4f3af153b
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-05-15 21:57:04 +0330

    feat(SpecialDiscounts) : dynamic button discound & VIP

--- src/components/Main/SpecialDiscounts.jsx
+++ src/components/Main/SpecialDiscounts.jsx
@@ -2,9 +2,9 @@ import { toPersianDigits } from "../../utils/ConvertToPersian"
 import { FcVip } from "react-icons/fc";
 
 const products = [
-    {id: 1, img: 'images/discound-product/img1.png' , name: 'سویشرت نایک مدل AO-14' , price: 300_000 , size: 'از سایز L تا XXL'},
-    {id: 2, img: 'images/discound-product/img2.jpg' , name: 'ست نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL'},
-    {id: 3, img: 'images/discound-product/img3.png', name: 'کفش نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL'},
+    {id: 1, img: '/public/images/discound-product/img2.jpg' , name: 'سویشرت نایک مدل AO-14' , price: 300_000 , size: 'از سایز L تا XXL' , discound: 0 , VIP: true},
+    {id: 2, img: 'images/discound-product/img3.png' , name: 'ست نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL' , discound: 24 , VIP: false},
+    {id: 3, img: 'images/discound-product/img1.png', name: 'کفش نایک مدل S-2000', price: 200_000, size: 'از سایز L تا XXL' , discound: 24 , VIP: false},
 ]
 
 const SpecialDiscounts = () => {
@@ -24,8 +24,20 @@ const SpecialDiscounts = () => {
                     products.map(product => (
 
                         <div className=" relative w-[366px] h-[467px] mt-20 bg-white rounded-4xl">
+                            {/* icon VIP */}
+                            {product.VIP && (
+                            <div className="absolute left-2 top-2.5 z-10">
+                                <FcVip className="w-10 h-10" />
+                            </div>
+                            )}
                             {/* icon discound */}
-                            <div className="absolute left-2 top-2.5 "><FcVip className="w-10 h-10" /></div>
+                            {product.discound > 0 && (
+                            <div className="absolute right-2 top-2.5 z-10 ml-12">
+                                <span className="bg-secondarySky w-10 h-10 p-1 rounded-full flex items-center justify-center text-white text-sm font-vazirB">
+                                %{toPersianDigits(product.discound)}
+                                </span>
+                            </div>
+                            )}
                             {/* img */}
                             <img src={product.img} alt="" className=" rounded-4xl" />
                             {/* details */}
